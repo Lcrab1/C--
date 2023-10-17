bfed47d3110bfb864438e4e8f1732db95f39e3ce
Asuka <1767731149@qq.com>
2023-10-17 17:24:06 +0800

    版本1

---
+++ test/max.h
@@ -0,0 +1,3 @@
+int Max(int a,int b){
+    return a>b?a:b;
+}
---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<tchar.h>
+#include "max.h"
+using namespace std;
+int main(){
+    cout<<"Hello,World!"<<endl;
+    cout<<"你好，世界!"<<endl;
+    cout<<Max(10,20)<<endl;
+    system("pause");
+}
