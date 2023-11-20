c66b4f933d5cfb0f50e44bc32c0c6509fd726578
Asuka <1767731149@qq.com>
2023-11-20 20:45:48 +0800

    correct answer

--- CSAPP/Chapter2,BitwiseOperations/2.42.cpp
+++ CSAPP/Chapter2,BitwiseOperations/2.42.cpp
@@ -4,14 +4,18 @@ using namespace std;
 //返回x/16的值
 int div16(int x)
 {
-    return (x < 0 ? x + (1 << 4) - 1 : x) >> 4;
+    //向右移位是算术移位
+    //向右移动31位来判断正负，若为负，则x>>31全为1，若为正，则x>>31全为0
+    //与掩码进行与运算，得到偏置量，偏置量bias=除数-1(16-1=15=0xF)
+    int bias = (x >> 31) & 0xF;
+    return (x + bias) >> 4;
 }
 
-int main()
-{
-    int x = 10;
-    int y = -17;
-    int z = 18;
-    cout << div16(x) << " " << div16(y) << " " << div16(z) << endl;
-    system("pause");
+    int main()
+    {
+        int x = 10;
+        int y = -17;
+        int z = 18;
+        cout << div16(x) << " " << div16(y) << " " << div16(z) << endl;
+        system("pause");
 }
